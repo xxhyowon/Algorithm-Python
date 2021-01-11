c8c6c53421213b981fabdcddf368c303c3f4f0a8
hyowon <xxchlgydnjs@gmail.com>
2021-01-11 21:35:16 +0900

    [add] 2021.01.11 에라토스테네스의 체

--- [015] 소수찾기.py
+++ [015] 소수찾기.py
@@ -1,7 +1,7 @@
 # 210108 소수찾기
 # 1부터숫자 n 사이에 있는 소수의 개수를 반환
 
-def solution(n): # 시간초과
+def solutionfail(n): # 시간초과
     count = n-1
     for i in range(2,n+1) :
         for j in range(2,i) :
@@ -10,3 +10,17 @@ def solution(n): # 시간초과
                 break
     answer = count
     return answer
+
+import math
+
+def solution(n): # 에라토스테네스의 체(sqrt(n) 이하의 수만 확인)
+    num = set(range(2,n+1)) # 2부터 n+1까지 모든 수들의 집합
+    for i in range(2, int(math.sqrt(n))+1): # 2부터 n제곱근까지 순회하면서
+        num -= set(range(i*2, n+1, i)) # 집합num에서 i 배수 제거
+    answer = len(num) # 집합num에 남아있는 수 == 소수
+    return answer
+
+#testcode
+print(solution(10)) #4
+print(solution(5)) #3
+print(solution(10000)) #1229
