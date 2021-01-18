e4916306339256e1344c8feae53277c6bafa705e
hyowon <xxchlgydnjs@gmail.com>
2021-01-18 22:51:56 +0900

    [add] 2021.01.18

---
+++ [021] 수식 최대화.py
@@ -0,0 +1,18 @@
+# 210118 수식 최대화
+
+def solution(expression):
+    operations = [('+', '-', '*'),('+', '*', '-'),('-', '+', '*'),('-', '*', '+'),('*', '+', '-'),('*', '-', '+')]
+    answer = []
+    for op in operations:
+        a = op[0]
+        b = op[1]
+        temp_list = []
+        for e in expression.split(a):
+            temp = [f"({i})" for i in e.split(b)]
+            temp_list.append(f'({b.join(temp)})')
+        answer.append(abs(eval(a.join(temp_list))))
+    return max(answer)
+
+#testcode
+print(solution("100-200*300-500+20")) #60420
+print(solution("50*6-3*2")) #300
